c054dc14e5519120aa8563c453656b15ab430f63
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2020-03-31 17:01:45 -0500

    CSV import improvements for properites

--- market_knowledge/app/models/property.rb
+++ market_knowledge/app/models/property.rb
@@ -16,9 +16,24 @@ class Property < ApplicationRecord
   # Update the record.
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-      properties_hash = row.to_hash
-      property = find_or_create_by!(property_name: properties_hash['property_name'])
-      property.update_attributes(properties_hash)
+      property_hash = row.to_hash
+      @property = Property.where(:property_name => property_hash["property_name"], :street => property_hash["street"], :city => property_hash[:city).first_or_create do |property|
+        property.year_built = property_hash["year_built"]
+        property.remodel_year = property_hash["remodel_year"]
+        property.gross_bldg_area = property_hash["gross_bldg_area"]
+        property.foundaiton_area = property_hash["foundation_area"]
+        property.num_stories = property_hash["num_stories"]
+        property.net_lease_area = property_hash["net_lease_area"]
+        property.bldg_class = property_hash["bldg_class"]
+        property.business_park = property_hash["business_park"]
+        property.submarket = property_hash["submarket"]
+        property.landlord_broker_company = property_hash["landlord_broker_company"]
+        property.bldg_owner = property_hash["bldg_owner"]
+        property.property_manager = property_hash["property_manager"]
+        property.quoted_rate = property_hash["quoted_rate"]
+        property.nnn = property_hash["nnn"]
+        property.electric = property_hash["electric"]
+      end
     end
   end
 
